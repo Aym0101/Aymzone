9f8da46a04d371aca119d978a792cc2efbe2fea7
Aym0101 <asefym7@gmail.com>
2026-01-08 21:16:11 +0430

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,73 @@
+// Service Worker for AYM Shop PWA
+const CACHE_NAME = 'aym-shop-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/styles.css',
+    '/js/database.js',
+    '/js/app.js',
+    '/images/logo.jpg',
+    '/images/header.jpg',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+    // Skip API calls
+    if (event.request.url.includes('/api/')) {
+        return;
+    }
+    
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                
+                return fetch(event.request).then(
+                    response => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        
+                        const responseToCache = response.clone();
+                        
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        
+                        return response;
+                    }
+                );
+            })
+    );
+});
